80378b1d82f44ac65718dc013a2428c1971e0a09
Elizeu Lopes <scarabellot.lopes@gmail.com>
2024-06-06 19:52:52 -0300

    add express session

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
+const NODE_SECRET = "4e6f8b975b73d4c366d2f601155af00f";
 const connection = require("./config/database");
+const { User, Category, Video } = require("./model");
 connection
   .authenticate()
   .then(() => {
     console.log("Connection made successfully.");
   })
   .catch((err) => console.log(err));
+app.use(
+  session({
+    secret: NODE_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 24 * 60 * 60 * 1000 },
+  })
+);
+
+app.use((req, res, next) => {
+  res.locals.session = req.session;
+  next();
+});
+
