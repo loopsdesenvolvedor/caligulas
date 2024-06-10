1f7f6fd05d50403e02fa9621bf29de5dd6f83455
Elizeu Lopes <scarabellot.lopes@gmail.com>
2024-06-10 01:04:32 -0300

    add function to add user

---
+++ config/addUsers.js
@@ -0,0 +1,24 @@
+const User = require("../model/User");
+const bcrypt = require("bcryptjs");
+
+async function addUser(email, password) {
+  try {
+    // Gerar um salt
+    const salt = await bcrypt.genSalt(10);
+
+    // Criptografar a senha
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    User.create({
+      email: email,
+      password: hashedPassword,
+    });
+
+    console.log("Usuário adicionado com sucesso!");
+  } catch (error) {
+    console.error("Erro ao adicionar usuário:", error);
+  }
+}
+
+// Exemplo de uso
+addUser("scarabellot.lopes@gmail.com", "120900**");
