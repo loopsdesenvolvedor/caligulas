9a2fe5f7feeb50ce58d6fdd1775789b040f9fb19
Elizeu Lopes <scarabellot.lopes@gmail.com>
2024-06-10 01:02:29 -0300

    add  models router

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const NODE_SECRET = "4e6f8b975b73d4c366d2f601155af00f";
 const connection = require("./config/database");
 const { User, Category, Video } = require("./model");
 var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+var usersRouter = require("./routes/user");
+var dashboardRouter = require("./routes/dashboard");
 
 var app = express();
 
@@ -41,6 +42,9 @@ app.use(
     cookie: { maxAge: 24 * 60 * 60 * 1000 },
   })
 );
+app.use("/", indexRouter);
+app.use("/", usersRouter);
+app.use("/", dashboardRouter);
 
 app.use((req, res, next) => {
   res.locals.session = req.session;
