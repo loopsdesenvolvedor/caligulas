02726b8f7bab215334991b9f93e297738e1dcca7
Elizeu Lopes <scarabellot.lopes@gmail.com>
2024-06-10 01:13:56 -0300

    add video search page by category

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,43 @@ router.get("/video/:id", async (req, res) => {
     .catch((err) => console.log(err));
 });
 
+router.get("/page/category/:slug", async (req, res) => {
+  let slug = req.params.slug;
+  const now = new Date();
+  const twentyFourHoursAgo = new Date(now.getTime() - 24 * 60 * 60 * 1000);
+
+  try {
+    const recentVideos = await Video.findAll({
+      where: {
+        createdAt: {
+          [Sequelize.Op.gte]: twentyFourHoursAgo,
+        },
+      },
+    });
+
+    const category = await Category.findOne({
+      where: {
+        slug: slug,
+      },
+      include: [{ model: Video }],
+    });
+
+    if (category != undefined) {
+      res.render("category", {
+        isCategory: true,
+        videos: category.videos,
+        title: category.name,
+        recentVideos: recentVideos,
+        isHome: false,
+        result: false,
+      });
+    } else {
+      res.redirect("/");
+    }
+  } catch (err) {
+    console.error(err);
+    res.redirect("/");
+  }
 });
 
 module.exports = router;
