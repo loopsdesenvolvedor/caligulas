8859c902bd18310f3e1ac588da56276bd212ce17
Elizeu Lopes <scarabellot.lopes@gmail.com>
2024-12-18 02:21:11 -0300

    feat: update font for poppins

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,10 @@
 import { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Poppins } from "next/font/google";
 import StyledComponentsRegistry from "@/lib/registry";
-import { Provider } from "react-redux";
-import store from "@/store";
-
-const geistSans = Geist({
+const poppins = Poppins({
   variable: "--font-geist-sans",
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["300", "400", "600", "800"],
 });
 
 export const metadata: Metadata = {
