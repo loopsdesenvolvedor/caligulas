c090dbd60a983fa08ee6714c291e1a950c73a887
Elizeu Lopes <scarabellot.lopes@gmail.com>
2024-12-18 02:22:48 -0300

    feat: add component header

---
+++ src/components/Header/header.styles.ts
@@ -0,0 +1,10 @@
+"use client";
+
+import styled from "styled-components";
+
+export const Root = styled.header`
+  width: 100%;
+  height: 60px;
+`;
+
+export const RootContent = styled.div``;
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,20 @@
+import Link from "next/link";
+import * as H from "./header.styles";
+
 import Container from "../Container";
+
+import Logo from "../Logo";
+
+const Header = () => {
+  return (
+    <H.Root>
       <Container>
+        <Link href="/">
+          <Logo width={100} height={100} />
+        </Link>
       </Container>
+    </H.Root>
+  );
+};
+
+export default Header;
