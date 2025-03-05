b3908a5b65d2a9b2c26aadec25a2a7ce8c589018
Elizeu <loopsdesenvolvedor@gmail.com>
2025-03-05 02:46:58 -0300

    feat: add theme to styled components default theme

--- src/@types/theme.d.ts
+++ src/@types/theme.d.ts
@@ -1,7 +1,6 @@
-import { Theme } from "./styles/theme";
-import 'styled-components'
+import Theme from "@/styles/theme";
+import { DefaultTheme } from "styled-components";
 
-declare module 'styled-components' {
-    export interface DefaultTheme extends Theme {}
+declare module "styled-components" {
+  export interface DefaultTheme extends Theme {}
 }
-
