deb771cf8b75402143d05149094b0d740475d086
Elizeu <loopsdesenvolvedor@gmail.com>
2025-03-05 02:49:47 -0300

    refactor:  breakpoint removed from theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 export const theme = {
   colors: {
@@ -6,20 +6,12 @@ export const theme = {
     yellow: "#ffc107",
     background: "#12191F",
     text: "#fff",
-    textLightWhite: "rgba(255,255,255,.8",
-    textLightBlack: "rgba(51,51,51,.8",
-    border: "rgba(255,255,255,.05",
+    textLightWhite: "rgba(255,255,255,.8)",
+    textLightBlack: "rgba(51,51,51,.8)",
+    border: "rgba(255,255,255,.05)",
   },
   fonts: {
     body: "'Poppins', sans-serif",
-
-
-  },
-  breakpoints: {
-    sm: "600px",
-    md: "900px",
-    lg: "1200px",
-    xl: "1800px",
   },
   spacing: {
     small: "8px",
