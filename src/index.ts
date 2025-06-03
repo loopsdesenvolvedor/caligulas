c4e823a195a2bc9ab5ab364f1b4e1fa9ffb9b0a2
Elizeu <loopsdesenvolvedor@gmail.com>
2025-06-02 23:03:04 -0300

    refactor:  changing settings for main route files

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,9 @@
 import express from "express";
-import path from "path";
 import { routes } from "./routes";
 
 const PORT = process.env.PORT;
 
 const app = express();
-
-app.use(express.static(path.join(__dirname, "..", "public")));
-app.set("view engine", "ejs");
-app.set("views", path.join(process.cwd(), "src/views"));
 
 routes(app);
 
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,11 @@
 import express from "express";
+import path from "path";
 import { homeRoute } from "./pages/home.routes";
 
 export const routes = (app: express.Express) => {
+  app.use(express.static(path.join(__dirname, "..", "..", "public")));
+  app.set("view engine", "ejs");
+  app.set("views", path.join(process.cwd(), "src/views"));
   app.use(express.json());
   app.use(homeRoute);
 };
