0be3130c500f93eef7271cdf2ad1b4dd05dddb7a
Elizeu <loopsdesenvolvedor@gmail.com>
2025-06-24 21:29:41 -0300

    feat: add dotenv

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import "express-async-errors";
 import cors from "cors";
+import dotenv from "dotenv";
 
 import { routes } from "./routes";
 import { errorMiddleware } from "./middlewares/error";
+
+dotenv.config();
 
 const app = express();
 app.use(cors());
