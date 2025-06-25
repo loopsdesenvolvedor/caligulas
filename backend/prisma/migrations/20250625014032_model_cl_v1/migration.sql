34516d98377c4bf81f00a436ef36a8cb189ba843
Elizeu <loopsdesenvolvedor@gmail.com>
2025-06-24 22:52:15 -0300

    feat: add user creation class

--- backend/prisma/migrations/20250625014032_model_cl_v1/migration.sql
+++ backend/prisma/migrations/20250625014032_model_cl_v1/migration.sql
@@ -1,5 +1,5 @@
 -- CreateTable
-CREATE TABLE `User` (
+CREATE TABLE `user` (
     `id` VARCHAR(191) NOT NULL,
     `name` VARCHAR(191) NULL,
     `username` VARCHAR(191) NULL,
@@ -9,8 +9,6 @@ CREATE TABLE `User` (
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
-    UNIQUE INDEX `User_username_key`(`username`),
-    UNIQUE INDEX `User_email_key`(`email`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -40,19 +38,19 @@ CREATE TABLE `posts` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `_PostCategories` (
+CREATE TABLE `_postcategories` (
     `A` VARCHAR(191) NOT NULL,
     `B` VARCHAR(191) NOT NULL,
 
-    UNIQUE INDEX `_PostCategories_AB_unique`(`A`, `B`),
-    INDEX `_PostCategories_B_index`(`B`)
+    UNIQUE INDEX `_postcategories_AB_unique`(`A`, `B`),
+    INDEX `_postcategories_B_index`(`B`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
-ALTER TABLE `posts` ADD CONSTRAINT `posts_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `posts` ADD CONSTRAINT `posts_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `user`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `_PostCategories` ADD CONSTRAINT `_PostCategories_A_fkey` FOREIGN KEY (`A`) REFERENCES `categories`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `_postcategories` ADD CONSTRAINT `_postcategories_A_fkey` FOREIGN KEY (`A`) REFERENCES `categories`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `_PostCategories` ADD CONSTRAINT `_PostCategories_B_fkey` FOREIGN KEY (`B`) REFERENCES `posts`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `_postcategories` ADD CONSTRAINT `_postcategories_B_fkey` FOREIGN KEY (`B`) REFERENCES `posts`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
--- backend/src/services/user/CreateUserServices.ts
+++ backend/src/services/user/CreateUserServices.ts
@@ -1,3 +1,6 @@
+import prisma from "../../lib/prisma";
+import { hash } from "bcrypt";
+
 type Types = {
   name: string;
   username: string;
@@ -7,7 +10,36 @@ type Types = {
 };
 
 class CreateUserService {
-  async execute({ name, username, email, password, avatar }: Types) {}
+  async execute({ name, username, email, password, avatar }: Types) {
+    const userAlreadyExists = await prisma.user.findFirst({
+      where: { email },
+    });
+
+    if (userAlreadyExists) {
+      throw new Error("User already exists");
+    }
+
+    const passwordHash = await hash(password, 8);
+
+    const user = await prisma.user.create({
+      data: {
+        name,
+        username,
+        email,
+        password: passwordHash,
+        avatar,
+      },
+
+      select: {
+        id: true,
+        name: true,
+        username: true,
+        email: true,
+      },
+    });
+
+    return user;
+  }
 }
 
 export { CreateUserService };
