929c75ed2e300b798c5d4abde2b8fe433a2df237
Elizeu <loopsdesenvolvedor@gmail.com>
2025-07-01 00:47:07 -0300

    fix(category): improve slug generation with strict and locale pt

--- backend/src/services/category/CreateCategoryService.ts
+++ backend/src/services/category/CreateCategoryService.ts
@@ -1,17 +1,17 @@
 import slugify from "slugify";
-import { CreateCategoryInput } from "../../@types/Category";
+import { CategoryInput } from "../../@types/Category";
 import prisma from "../../lib/prisma";
 
 class CreateCategoryService {
-  async execute({ name }: CreateCategoryInput) {
+  async execute({ name }: CategoryInput) {
     if (!name) {
       throw new Error("Invalid name error");
     }
 
     const category = await prisma.category.create({
       data: {
-        name: name.toLowerCase(),
-        slug: slugify(name),
+        name: name,
+        slug: slugify(name, { lower: true, strict: true, locale: "pt" }),
       },
     });
 
