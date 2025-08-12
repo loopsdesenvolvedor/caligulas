11ae54de8d554c21982f2947c47f505ae894bb2a
Elizeu <loopsdesenvolvedor@gmail.com>
2025-08-12 19:42:49 -0300

    feat: add language pt-BR

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <html lang="pt-BR">
       <body className={`${interSans.variable} antialiased`}>
         <Header />
         {children}
