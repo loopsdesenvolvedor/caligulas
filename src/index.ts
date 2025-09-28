6def9d92b80501beae6f7b555b4f6d173950b120
Elizeu <loopsdesenvolvedor@gmail.com>
2025-09-28 16:33:21 -0300

    feat: add express-async-errors for async error handling

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import express from "express";
+
 import dotenv from "dotenv";
+
+import routes from "./routes/index.js";
 
 dotenv.config();
 const port = process.env.PORT;
 
 const app = express();
 
+routes(app);
+
 app.listen(port, () => {
-  console.log(`App is running on port:${port}`);
+  console.info(`App is running on port:${port}`);
 });
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,22 @@
-import express from "express";
+import express, {
+  type NextFunction,
+  type Request,
+  type Response,
+} from "express";
+import "express-async-errors";
+
 import userRoutes from "./user/userRoutes.js";
 
 export default (app: express.Express) => {
   app.use(express.json());
   app.use(userRoutes);
+
+  app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    if (err instanceof Error) res.status(400).json({ message: err.message });
+
+    res.status(500).json({
+      status: "Error",
+      message: "Internal server error",
+    });
+  });
 };
