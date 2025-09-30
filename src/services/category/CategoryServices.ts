c72e1c33edddfbaff1db60e4e79b5edf040e299b
Elizeu <loopsdesenvolvedor@gmail.com>
2025-09-30 15:47:44 -0300

    feat: add logic to search all categories

--- src/services/category/CategoryServices.ts
+++ src/services/category/CategoryServices.ts
@@ -2,6 +2,16 @@ import type { CreateCategoryProps } from "../../@types/Category.js";
 import prisma from "../../lib/prisma.js";
 
 class CategoryServices {
+  async getAll() {
+    const cateories = await prisma.category.findMany();
+
+    if (cateories.length === 0) {
+      throw new Error("Nenhuma categoria encontrada");
+    }
+
+    return cateories;
+  }
+
   async create({ name, slug }: CreateCategoryProps) {
     if (!name || !slug) {
       throw new Error("Nome e slug são campos obrigatório.");
