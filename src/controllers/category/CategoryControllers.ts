cd4acb3e006ed07c236e93193f80b09dcbb0722d
Elizeu <loopsdesenvolvedor@gmail.com>
2025-09-30 15:47:44 -0300

    feat: add logic to search all categories

--- src/controllers/category/CategoryControllers.ts
+++ src/controllers/category/CategoryControllers.ts
@@ -4,6 +4,19 @@ import { BaseController } from "../baseController.js";
 import { CategoryServices } from "../../services/category/CategoryServices.js";
 
 class CategoryControllers extends BaseController {
+  async getAll() {
+    try {
+      const categoryServices = new CategoryServices();
+      const categoires = await categoryServices.getAll();
+
+      return this.res.status(200).json(categoires);
+    } catch (error: any) {
+      this.res.status(400).json({
+        message: error.message || "Erro ao buscar as categorias",
+      });
+    }
+  }
+
   async create() {
     try {
       const { name } = this.req.body;
--- src/routes/category/index.ts
+++ src/routes/category/index.ts
@@ -3,6 +3,10 @@ import { CategoryControllers } from "../../controllers/category/CategoryControll
 
 const categoryRoutes = Router();
 
+categoryRoutes.get("/category", (req, res, next) => {
+  new CategoryControllers(req, res, next).getAll();
+});
+
 categoryRoutes.post("/category", (req, res, next) => {
   new CategoryControllers(req, res, next).create();
 });
