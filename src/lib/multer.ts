132d2689bac36c912cec5999071af1ed982f29a9
Elizeu <loopsdesenvolvedor@gmail.com>
2025-10-10 03:00:40 -0300

    feat(upload): separate image and video uploads into distinct folders

--- src/lib/multer.ts
+++ src/lib/multer.ts
@@ -2,21 +2,35 @@ import multer from "multer";
 import { resolve, dirname } from "path";
 import { randomBytes } from "crypto";
 import { fileURLToPath } from "url";
+import fs from "fs";
 
 const __fileName = fileURLToPath(import.meta.url);
 const __dirname = dirname(__fileName);
 
 export default {
-  upload(folder: string) {
-    return {
+  upload() {
+    return multer({
       storage: multer.diskStorage({
-        destination: resolve(__dirname, "..", "..", folder),
-        filename: (request, file, callback) => {
+        destination: (req, file, cb) => {
+          let folder = "";
+
+          if (file.fieldname === "image") folder = "uploads/images";
+          if (file.fieldname === "video") folder = "uploads/videos";
+
+          const fullPath = resolve(__dirname, "..", "..", folder);
+
+          if (!fs.existsSync(fullPath)) {
+            fs.mkdirSync(fullPath, { recursive: true });
+          }
+
+          cb(null, fullPath);
+        },
+        filename: (req, file, cb) => {
           const fileHash = randomBytes(16).toString("hex");
           const fileName = `${fileHash}-${file.originalname}`;
-          callback(null, fileName);
+          cb(null, fileName);
         },
       }),
-    };
+    });
   },
 };
